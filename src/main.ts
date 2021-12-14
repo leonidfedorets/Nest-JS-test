f4f382a16a269d960d147e349360dbc1ada6a7a7
leo <leonidfedorets30@gmai.com>
2021-12-14 22:28:52 +0200

    updating the engine template

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { join, resolve } from 'path-posix';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const APP_DIRECTORY=resolve(__dirname,'..');
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  app.setViewEngine ('hbs');
+  app.setBaseViewsDir (resolve(APP_DIRECTORY,'views'));
+  app.useStaticAssets (resolve(APP_DIRECTORY,'public'));
+
   await app.listen(3000);
 }
 bootstrap();
