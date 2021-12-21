99ef6f21c491b282239e7ef94f5f27b0b6064749
leo <leonidfedorets30@gmai.com>
2021-12-21 21:16:07 +0200

    All others

---
+++ src/location/location.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from '../app.service';
+
+interface ILocationListDto {
+  locations: string[];
+}
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('list.hbs')
+  listLocations(): ILocationListDto {
+    return {
+      locations:[
+'Location 1',
+'Location 2',
+'Location 3',
+      ],
+    };
+  }
+}
